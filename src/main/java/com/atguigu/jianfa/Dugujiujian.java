ba390d8b62b3dfbbd60f88e038c2bb967e194ff7
skygzx <979915941@qq.com>
2020-11-04 23:06:06 +0800

    新增

--- src/main/java/com/atguigu/jianfa/Dugujiujian.java
+++ src/main/java/com/atguigu/jianfa/Dugujiujian.java
@@ -2,6 +2,7 @@ package com.atguigu.jianfa;
 
 public class Dugujiujian {
     public static void main(String[] args) {
-
+        System.out.println("葵花点穴收");
+        System.out.println("嘻嘻嘻");
     }
 }
